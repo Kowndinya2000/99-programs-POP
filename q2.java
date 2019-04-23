42ea302e8716945af0f040fe3aefb87c096aa54d
Kowndinya2000 <46968076+Kowndinya2000@users.noreply.github.com>
2019-04-23 20:43:43 +0530

    Update q2.java

--- q2.java
+++ q2.java
@@ -7,18 +7,16 @@ public class GFG1 {
 
 		try { 
 
-			// Creating object of AbstractSet<Integer> 
+			
 			AbstractSet<Integer> 
 				abs_set = new TreeSet<Integer>(); 
 
-			// Populating abs_set 
+		
 			abs_set.add(1); 
 			abs_set.add(2); 
 			abs_set.add(3); 
 			abs_set.add(4); 
 			abs_set.add(5); 
-
-			// print abs_set 
 			System.out.println("AbstractSet: "
 							+ abs_set); 
 		} 

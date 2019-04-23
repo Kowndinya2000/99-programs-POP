c3e536706098364f6ae4c176ebe9f938375c4db7
Kowndinya2000 <46968076+Kowndinya2000@users.noreply.github.com>
2019-04-23 20:38:29 +0530

    Update q25.java

--- q25.java
+++ q25.java
@@ -1,12 +1,7 @@
-// Java program to illustrate 
-// scope of Initializing variables 
-// within the loop 
 public class Example5 
 { 
 	public static void main(String[] args) 
-	{ 
-		// x and y scope is only 
-		// within for loop 
+	{
 		for(int x = 0, y = 0; x < 3 && y < 3; x++, y++) 
 		{ 
 			System.out.println(y + " "); 

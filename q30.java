eed59d5cadea4423455901a5337e279dc9f7f058
Kowndinya2000 <46968076+Kowndinya2000@users.noreply.github.com>
2019-04-23 20:35:12 +0530

    Update q30.java

--- q30.java
+++ q30.java
@@ -1,10 +1,10 @@
-// Java program to demonstrate working of Collections.sort() 
+
 import java.util.*; 
 
 public class GFG { 
 	public static void main(String[] args) 
 	{ 
-		// Create a list of strings 
+		
 		ArrayList<String> al = new ArrayList<String>(); 
 		al.add("Geeks For Geeks"); 
 		al.add("Friends"); 
@@ -12,11 +12,7 @@ public class GFG {
 		al.add("Is"); 
 		al.add("Superb"); 
 
-		/* Collections.sort method is sorting the 
-		elements of ArrayList in ascending order. */
 		Collections.sort(al); 
-
-		// Let us print the sorted list 
 		System.out.println("List after the use of"
 						+ " Collection.sort() :\n" + al); 
 	} 

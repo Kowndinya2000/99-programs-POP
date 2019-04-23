864c32bb4cbc89ebe5fb1dc899799a7233732d61
Kowndinya2000 <46968076+Kowndinya2000@users.noreply.github.com>
2019-04-23 20:42:21 +0530

    Update q21.java

--- q21.java
+++ q21.java
@@ -1,23 +1,13 @@
-// A sample Java program to demonstrate use and 
-// working of blank final 
 class Test 
 { 
-	// We can initialize here, but if we 
-	// initialize here, then all objects get 
-	// the same value. So we use blank final 
 	final int i; 
 
 	Test(int x) 
 	{ 
-		// Since we have initialized above, we 
-		// must initialize i in constructor. 
-		// If we remove this line, we get compiler 
-		// error. 
 		i = x; 
 	} 
 } 
 
-// Driver Code 
 class Main 
 { 
 	public static void main(String args[]) 

f278ab2a2e5a32b2ce2d720f1fa8dd425bc5b271
Kowndinya2000 <46968076+Kowndinya2000@users.noreply.github.com>
2019-04-23 20:34:32 +0530

    Update q4.java

--- q4.java
+++ q4.java
@@ -1,5 +1,3 @@
-
-// Java program to demonstrate working of HashSet 
 import java.util.*; 
 
 class Test 
@@ -8,22 +6,22 @@ class Test
 	{ 
 		HashSet<String> h = new HashSet<String>(); 
 
-		// Adding elements into HashSet usind add() 
+		
 		h.add("India"); 
 		h.add("Australia"); 
 		h.add("South Africa"); 
-		h.add("India");// adding duplicate elements 
+		h.add("India");
 
-		// Displaying the HashSet 
+		
 		System.out.println(h); 
 		System.out.println("List contains India or not:" + 
 						h.contains("India")); 
 
-		// Removing items from HashSet using remove() 
+		
 		h.remove("Australia"); 
 		System.out.println("List after removing Australia:"+h); 
 
-		// Iterating over hash set items 
+		
 		System.out.println("Iterating over list:"); 
 		Iterator<String> i = h.iterator(); 
 		while (i.hasNext()) 
